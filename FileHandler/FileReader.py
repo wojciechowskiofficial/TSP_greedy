6b39b3781042eeea4091cef7d68492b4fa2f8ede
adam wojciechowski <adi61044@gmail.com>
2019-10-31 19:26:09 +0100

    Vertex class __str__ and __repr__ added, Vertex.setPos modified, FileReader.importInstance finished

--- FileHandler/FileReader.py
+++ FileHandler/FileReader.py
@@ -1,4 +1,5 @@
 import os
+from TSPCoreComponents.Vertex import Vertex
 
 class FileReader:
     def __init__(self, fileName):
@@ -11,8 +12,19 @@ class FileReader:
             os.makedirs('IOFiles')
         self.filePath = 'IOFiles/' + self.fileName
 
+
     def importInstance(self):
-        with open(self.filePath, 'r') as inFile:
-            instanceSize = inFile.readline()
+        with open(self.filePath, 'r') as f:
+            instanceSize = int(next(f))
+
+            def readPos():
+                tmpString = f.readline().split()
+                return [int(tmpString[1]), int(tmpString[2])]
+
             for i in range(instanceSize):
-                self.instance.append()
+                tmpVertex = Vertex()
+                tmpVertex.setPos(readPos())
+                tmpVertex.id = i
+                self.instance.append(tmpVertex)
+
+        return self.instance
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 from FileHandler.FileReader import FileReader
+from TSPCoreComponents.Vertex import Vertex
 
 def Main():
-    klasa = FileReader('nazwa')
+    scan = FileReader('test_instance.txt')
+    x = scan.importInstance()
+    print(x)
 
 if __name__ == '__main__':
     Main()
