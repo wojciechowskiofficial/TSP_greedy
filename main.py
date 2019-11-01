0b21a5aee63710fb8dd0efe630b1f3b6a2d7b797
adam wojciechowski <adi61044@gmail.com>
2019-11-01 13:06:53 +0100

    finished greedy algorithm with reusable TSPCoreComponents and FileHandler packages

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import os
 from FileHandler.FileReader import FileReader
-from TSPCoreComponents.Vertex import Vertex
+from TSPGreedy.Greedy import Greedy
+from TSPGreedy.SolutionContainer import SolutionContainer
 
 def Main():
     scan = FileReader('test_instance.txt')
     x = scan.importInstance()
-    print(x)
+    greed = Greedy(x)
+    greed.solveGreedy()
+    print(greed.solution)
 
 if __name__ == '__main__':
     Main()
