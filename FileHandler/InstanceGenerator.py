899eb336ea791d03fb980a61c7621ae84ccc7912
adam wojciechowski <adi61044@gmail.com>
2019-11-01 23:00:16 +0100

    FileHandler.InstanceGenerator class fully functional

--- FileHandler/InstanceGenerator.py
+++ FileHandler/InstanceGenerator.py
@@ -4,13 +4,14 @@ import os
 class InstanceGenerator:
     def __init__(self, size, constrains = None):
         self.size = size
-        if constrains == None:
+        if constrains is None:
             self.constrains = size * 1000
         else:
             self.constrains = constrains
         self.fileName = None
+        self.pathName = None
 
-    def AssembleFileName(self):
+    def assembleFileName(self):
         string = 'gen_size_'
         string += str(self.size)
         string += '_constrains_'
@@ -21,14 +22,26 @@ class InstanceGenerator:
     def createPath(self):
         if not os.path.exists('IOFiles'):
             os.makedirs('IOFiles')
+        #possible error when self.fileName was not created beforehand
         try:
             self.pathName = 'IOFiles/' + self.fileName
         except TypeError:
-            self.AssembleFileName()
+            self.assembleFileName()
             self.pathName = 'IOFiles/' + self.fileName
 
     def generate(self):
-        if self.fileName == None:
-            self.AssembleFileName()
-        with open(self.fileName, 'w') as f:
-            pass
+        if self.fileName is None:
+            self.assembleFileName()
+        if self.pathName is None:
+            self.createPath()
+        with open(self.pathName, 'w') as f:
+            f.write(str(self.size))
+            f.write('\n')
+            for i in range(self.size):
+                f.write(str(i + 1))
+                f.write(' ')
+                f.write(str(random.randint(0, self.constrains)))
+                f.write(' ')
+                f.write(str(random.randint(0, self.constrains)))
+                f.write('\n')
+
