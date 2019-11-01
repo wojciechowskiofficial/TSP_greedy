0785395a7631bf8a9c0c31147bca9e349a4af4b6
adam wojciechowski <adi61044@gmail.com>
2019-11-01 23:11:34 +0100

    project rename __pycache__ update

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from FileHandler.FileReader import FileReader
-from TSPGreedy.Greedy import Greedy
-from TSPGreedy.SolutionContainer import SolutionContainer
-
 def Main():
-    scan = FileReader('test_instance.txt')
-    x = scan.importInstance()
-    greed = Greedy(x)
-    greed.solveGreedy()
-    print(greed.solution)
+    pass
 
 if __name__ == '__main__':
     Main()
